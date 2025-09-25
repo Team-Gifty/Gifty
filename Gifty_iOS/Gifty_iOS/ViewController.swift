aef9227c7a50cfc5a4e4305b54041a7c5c3c0992
Juhui <joohee090331@gmail.com>
2025-09-26 00:59:52 +0900

    setting :: 프로젝트 세팅

---
+++ Gifty_iOS/Gifty_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gifty_iOS
+//
+//  Created by 신주희 on 9/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
