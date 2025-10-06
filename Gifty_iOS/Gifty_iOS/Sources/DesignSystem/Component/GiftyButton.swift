3ba7a5ceeeba3ad27fb6212a2b519f7789146ef1
Juhui <joohee090331@gmail.com>
2025-10-07 02:32:29 +0900

    🐛 :: GiftyButton 컴포넌트 버그 수정
    비활성화 상태 setTitleColor 추가

--- Gifty_iOS/Gifty_iOS/Sources/DesignSystem/Component/GiftyButton.swift
+++ Gifty_iOS/Gifty_iOS/Sources/DesignSystem/Component/GiftyButton.swift
@@ -5,21 +5,21 @@ import Then
 public class GiftyButton: UIButton {
     public var buttonTap: (() -> Void)?
     public override var isEnabled: Bool {
-
+        
         didSet {
             attribute()
         }
     }
-
+    
     private var bgColor: UIColor {
         isEnabled ? UIColor(named: "FDE1AD") ?? .systemBrown : UIColor(named: "FCEDD0") ?? .lightGray
     }
-
+    
     override public init(frame: CGRect) {
         super.init(frame: frame)
         setupButton()
     }
-
+    
     convenience public init(
         type: UIButton.ButtonType? = .system,
         buttonText: String? = String(),
@@ -36,28 +36,38 @@ public class GiftyButton: UIButton {
             $0.height.equalTo(height ?? 0)
         }
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     public override func layoutSubviews() {
         super.layoutSubviews()
         attribute()
     }
-
+    
     private func setupButton() {
         addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
         attribute()
     }
-
+    
     @objc private func buttonTapped() {
         buttonTap?()
     }
-
+    
     public func attribute() {
+        
         self.backgroundColor = bgColor
-        self.setTitleColor(UIColor(named: "A98E5C") ?? .white, for: .normal)
+        
+        // 상태에 따라 글자색 변경
+        let titleColor: UIColor = isEnabled
+        ? UIColor(named: "A98E5C") ?? .white        // 활성
+        : UIColor(named: "DDC495") ?? .lightGray   // 비활성
+        
+        self.setTitleColor(titleColor, for: .normal)
+        
+        self.backgroundColor = bgColor
+        //   self.setTitleColor(UIColor(named: "A98E5C") ?? .white, for: .normal)
         self.titleLabel?.font = .onboardingFont(size: 23)
         self.layer.cornerRadius = 8
     }
