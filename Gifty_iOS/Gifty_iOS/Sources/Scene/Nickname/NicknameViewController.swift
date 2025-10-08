6df4426af44026f974c0cf84fab164b77ba52dda
jihoon <leejh080628@naver.com>
2025-10-09 04:47:39 +0900

    🔖 :: 중복 함수 해결

--- Gifty_iOS/Gifty_iOS/Sources/Scene/Nickname/NicknameViewController.swift
+++ Gifty_iOS/Gifty_iOS/Sources/Scene/Nickname/NicknameViewController.swift
@@ -111,18 +111,3 @@ class NicknameViewController: BaseViewController {
         nicknameButton.setTitleColor(textColor, for: .normal)
     }
 }
-
-extension NicknameViewController: UITextFieldDelegate {
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        let currentText = textField.text ?? ""
-        let newText = (currentText as NSString).replacingCharacters(in: range, with: string)
-        
-        if newText.count < 1 {
-            updateNicknameButtonState(isEnabled: false, backgroundColor: .FCEDD_0, textColor: .DDC_495)
-        } else {
-            updateNicknameButtonState(isEnabled: true, backgroundColor: .FDE_1_AD, textColor: .A_98_E_5_C)
-        }
-        
-        return true
-    }
-}
