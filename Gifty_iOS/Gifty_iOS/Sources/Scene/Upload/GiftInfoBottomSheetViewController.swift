935eaf4522ca284dfb6e2914b62af0fe5eeb0640
jihoon <leejh080628@naver.com>
2025-10-15 19:29:07 +0900

    🔖 :: 바텀시트 키보드 처리

--- Gifty_iOS/Gifty_iOS/Sources/Scene/Upload/GiftInfoBottomSheetViewController.swift
+++ Gifty_iOS/Gifty_iOS/Sources/Scene/Upload/GiftInfoBottomSheetViewController.swift
@@ -26,8 +26,19 @@ class GiftInfoBottomSheetViewController: UIViewController {
         
         addView()
         setLayout()
+        hideKeyboardWhenTappedAround()
         
         saveButton.addTarget(self, action: #selector(saveButtonTapped), for: .touchUpInside)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        addKeyboardObservers()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        removeKeyboardObservers()
     }
     
     private func addView() {
@@ -59,6 +70,49 @@ class GiftInfoBottomSheetViewController: UIViewController {
             $0.top.equalTo(memoTextField.snp.bottom).offset(40)
             $0.leading.trailing.equalToSuperview().inset(20)
             $0.bottom.equalToSuperview().inset(40)
+        }
+    }
+    
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    private func addKeyboardObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    private func removeKeyboardObservers() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        guard let userInfo = notification.userInfo,
+              let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else {
+            return
+        }
+
+        let keyboardHeight = keyboardFrame.height
+        
+        saveButton.snp.updateConstraints {
+            $0.bottom.equalToSuperview().inset(keyboardHeight + 20)
+        }
+        
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+
+    @objc private func keyboardWillHide(_ notification: Notification) {
+        saveButton.snp.updateConstraints {
+            $0.bottom.equalToSuperview().inset(40)
+        }
+        
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
         }
     }
     
