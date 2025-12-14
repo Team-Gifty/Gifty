84ea8633598a1411b7ea9e44bd00f1fe183f0250
jihoon <leejh080628@naver.com>
2025-12-15 06:03:19 +0900

    🔖 :: (#55) 구조 정리

--- Gifty_iOS/Gifty/Sources/Scene/Upload/Extension/CombineExtensions.swift
+++ Gifty_iOS/Gifty/Sources/Scene/Upload/Extension/CombineExtensions.swift
@@ -1,7 +1,6 @@
 import Foundation
 import Combine
 
-// MARK: - Publisher Extensions
 extension Publisher {
     func withLatestFrom<Other: Publisher>(_ other: Other) -> AnyPublisher<Other.Output, Self.Failure> where Other.Failure == Self.Failure {
         let upstream = self
